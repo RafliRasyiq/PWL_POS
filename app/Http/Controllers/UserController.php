79e7013fc1998afdc2495ad8b5ca5fcb2632a58d
Rafli Rasyiq <gaplekrasyiq@gmail.com>
2024-09-23 10:58:19 +0700

    JS04 Pratikum 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,7 +40,36 @@ class UserController extends Controller
         // $user = UserModel::findOrFail(1);
         // $user = UserModel::where('level_id', 2)->count();
         // dd($user);
-        $user = UserModel::where('level_id')->count();
+        // $user = UserModel::where('level_id')->count();
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ],
+        // );
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ],
+        // );
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
         return view('user', ['data' => $user]);
     }
 }
